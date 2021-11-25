2227647063752f9c3a403f19048fdf8fe2ea66fd
SeungsooChoi <tmdtn5389@gmail.com>
2021-11-25 14:06:01 +0900

    add : 8장 Hooks 추가

---
+++ hooks-tutorial/src/Counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{value}</b>입니다.
+      </p>
+      {/* setValue에 파라미터를 넣어서 호출하면 전달받은 파라미터로 값이 바뀌고 컴포넌트가 정상적으로 리렌더링함. */}
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
