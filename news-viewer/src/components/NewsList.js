26be273a824c397bf96b36b4953f52685c9feccd
SeungsooChoi <tmdtn5389@gmail.com>
2021-12-01 22:04:28 +0900

    test : news-viewer 프로젝트 추가, newsList 가져옴

---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+require('dotenv').config();
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+
+  useEffect(() => {
+    // async 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`,
+        );
+        setArticles(response.data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return loading ? (
+    <NewsListBlock>대기 중...</NewsListBlock>
+  ) : (
+    <NewsListBlock>
+      {articles &&
+        articles.map((article) => (
+          <NewsItem key={article.url} article={article} />
+        ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
