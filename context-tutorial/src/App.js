2434a5ec5c29c28499a8e90a24341da0528b9935
SeungsooChoi <tmdtn5389@gmail.com>
2021-12-03 23:58:59 +0900

    feat : 동적 Context 사용 진행중 - 코드이해중

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,14 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color";
+import { ColorProvider } from "./contexts/color";
 
 function App() {
   return (
-    //  Provider는 사용했는데 value를 명시하지 않는다면 오류가 발생한다!!
-    <ColorContext.Provider value={{ color: "red" }}>
+    <ColorProvider>
       <ColorBox />
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 }
 
