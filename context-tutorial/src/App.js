4f9b8739e253ce5ebf761acfa1f8431a6e422ad2
SeungsooChoi <tmdtn5389@gmail.com>
2021-12-03 23:49:35 +0900

    docs : Provider 주석추가

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -5,6 +5,7 @@ import ColorContext from "./contexts/color";
 
 function App() {
   return (
+    //  Provider는 사용했는데 value를 명시하지 않는다면 오류가 발생한다!!
     <ColorContext.Provider value={{ color: "red" }}>
       <ColorBox />
     </ColorContext.Provider>
